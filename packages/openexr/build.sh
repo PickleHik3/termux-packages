6b02ab108960f8e6173ff3f0e24aefc5765bd00e
PickleHik3 <vajdan.amal@live.com>
2026-06-30 10:09:25 +0300

    Phase 5: repair OpenEXR OpenJPH runtime payload

--- packages/openexr/build.sh
+++ packages/openexr/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Provides the specification and reference implementation 
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.4.4"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL="https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=7c663c3c41da9354b5af277bc2fd1d2360788050b4e0751a32bcd50e8abaef8f
 TERMUX_PKG_DEPENDS="imath, libc++, zlib"
@@ -25,6 +26,34 @@ termux_step_pre_configure() {
 	if [[ "$TERMUX_PKG_API_LEVEL" -lt 28 ]]; then
 		CPPFLAGS+=" -Daligned_alloc=memalign"
 	fi
+}
+
+termux_step_post_make_install() {
+	shopt -s nullglob
+
+	local _openjph_stage_dir="$TERMUX_PKG_TMPDIR/openjph-runtime-restage"
+	mkdir -p "$_openjph_stage_dir"
+
+	local -a _openjph_sources=(
+		"$TERMUX_PKG_BUILDDIR"/_deps/openjph-build/src/core/libopenjph.so*
+		"$TERMUX_PREFIX"/lib/libopenjph.so*
+		"$TERMUX_PREFIX"/lib64/libopenjph.so*
+	)
+
+	local _openjph_source
+	for _openjph_source in "${_openjph_sources[@]}"; do
+		cp -a "${_openjph_source}" "$_openjph_stage_dir/"
+	done
+
+	local -a _openjph_libs=("$_openjph_stage_dir"/libopenjph.so*)
+	[[ ${#_openjph_libs[@]} -gt 0 ]] || termux_error_exit "libopenjph runtime payload not found after install."
+
+	local _openjph_lib
+	for _openjph_lib in "${_openjph_libs[@]}"; do
+		install -Dm600 "${_openjph_lib}" "$TERMUX_PREFIX/lib/$(basename "$_openjph_lib")"
+	done
+
+	shopt -u nullglob
 }
 
 termux_step_post_massage() {
